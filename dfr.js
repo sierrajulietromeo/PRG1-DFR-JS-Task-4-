75336b63570837a7477c7327f1c8577884560878
Steve Rich <steven.john.rich@gmail.com>
2024-10-19 17:16:01 +0000

    Prettier'd, convertFloat renamed convertNumber, Tests tidied up and minor tweaks to the README.

--- dfr.js
+++ dfr.js
@@ -1,71 +1,55 @@
-const fs = require('fs'); 
+const fs = require("fs");
 
-
-function fileExists(filename) {
-  // returns true or false
+function fileExists(fileName) {
+  return fs.existsSync(fileName);
 }
 
-
-function validNumber(value) { // value can be string or numeric
- // returns a boolean 
+function validNumber(value) {
+  // value can be string or numeric
+  // returns a boolean
 }
-
 
 function dataDimensions(dataframe) {
-  // returns a list [(int), (int)]
+  // returns an array [ number, number ]
 }
-
 
 function calculateMean(dataset) {
-  // returns a float or false
-  
+  // returns a number
 }
-
 
 function findTotal(dataset) {
   // returns float or false
-  
-} 
-
-
-function convertToFloat(dataframe, col){ //dataframe, integer
-  // returns an integer, which is the number that were  converted to floats.
-  
 }
 
+function convertToNumber(dataframe, col) {
+  // returns a number, which is the number that were  converted to datatype number.
+}
 
 function flatten(dataframe) {
   // returns a dataset (a flattened dataframe)
-  
 }
 
-
-function loadCSV(csvFile, ignorerows, ignorecols) {  // string, dataset, dataset
+function loadCSV(csvFile, ignorerows, ignorecols) {
   // returns a list comprising of [dataframe, rows (integer), cols (integer)]
-
 }
-
 
 function calculateMedian(dataset) {
-  // return float or false 
-  
+  // return float or false
 }
 
-
-function createSlice(dataframe, colindex, colpattern, exportcols = []) { // dataframe, integer, string/numeric, dataset
+function createSlice(dataframe, colindex, colpattern, exportcols = []) {
   // returns a dataframe
-  
 }
-
-
-
-
-
-
-
-
 
 module.exports = {
-  fileExists, validNumber, dataDimensions, calculateMean, findTotal, convertToFloat, flatten, 
-  loadCSV, calculateMedian, createSlice,
-} 
+  fileExists,
+  validNumber,
+  dataDimensions,
+  calculateMean,
+  findTotal,
+  convertToNumber,
+  flatten,
+  loadCSV,
+  calculateMedian,
+  createSlice,
+};
